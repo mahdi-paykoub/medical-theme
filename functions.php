d873f5066de638a3f160b2c4c1ad6d55e4a19029
mahdipaykoub <mahdi.paykoub.github@gmail.com>
2023-03-13 09:51:18 +0330

    fix files

--- functions.php
+++ functions.php
@@ -2,15 +2,11 @@
 //partials
 include get_template_directory() . '/partials/config.php';
 include FILE_DIR . '/partials/load-files.php';
-include FILE_DIR . '/partials/add-menus.php';
+
 //add files inc
 include FILE_DIR . '/inc/shortcodes.php';
 include FILE_DIR . '/inc/ajax.php';
-include FILE_DIR . '/inc/meta-boxes.php';
-include FILE_DIR . '/inc/meta-taxonomy.php';
 include FILE_DIR . '/inc/widgets/widget-main-page.php';
-include FILE_DIR . '/inc/custom-post-type.php';
-include FILE_DIR . '/inc/admin/admin-menu.php';
 //cmb2
 require_once dirname(__FILE__) . '/CMB2-develop/init.php';
 require_once dirname(__FILE__) . '/cmb2/cmb2_options.php';
@@ -23,3 +19,10 @@ include FILE_DIR . '/partials/save-post.php';
 include FILE_DIR . '/partials/search-filter.php';
 include FILE_DIR . '/partials/validations.php';
 include FILE_DIR . '/partials/view-counter.php';
+if (is_admin()) {
+    include FILE_DIR . '/partials/add-menus.php';
+    include FILE_DIR . '/inc/meta-boxes.php';
+    include FILE_DIR . '/inc/meta-taxonomy.php';
+    include FILE_DIR . '/inc/admin/admin-menu.php';
+    include FILE_DIR . '/inc/custom-post-type.php';
+}
